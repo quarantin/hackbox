409a5d7b0db8bf71af7226236563fe9494b64c4e
Corentin Delorme <codelorme@gmail.com>
2019-07-17 14:26:32 +0200

    Update blacklist/management/commands/msf.py: Check for BDF Proxy resource file before starting msfconsole

--- blacklist/management/commands/msf.py
+++ blacklist/management/commands/msf.py
@@ -6,5 +6,14 @@ class Command(BaseCommand):
 
 	help = 'Run Metasploit Framework .'
 
+	def check_bdf_resource_file():
+		return os.path.exists('/home/hx/bdfproxy/bdfproxy_msf_resource.rc')
+
 	def handle(self, *args, **options):
+
+		if not check_bdf_resource_file():
+			print('You need to start BDF Proxy first: python3 manage.py bdf_proxy')
+			return
+
+		os.chdir('/opt/msf')
 		subprocess.run([ 'msfconsole', '-r', '/home/hx/bdfproxy/bdfproxy_msf_resource.rc' ])
