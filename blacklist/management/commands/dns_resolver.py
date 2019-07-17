3af278d1b7e6c909c9387102a1c17c01a92e04b1
Corentin Delorme <codelorme@gmail.com>
2019-07-17 17:07:36 +0200

    Update blacklist/management/commands/dns_resolver.py: Restart avahi-daemon

--- blacklist/management/commands/dns_resolver.py
+++ blacklist/management/commands/dns_resolver.py
@@ -1,22 +1,17 @@
 from django.core.management.base import BaseCommand, CommandError
 from blacklist.models import *
 
-from scapy.all import *
-from socket import AF_INET, SOCK_DGRAM, socket
-from traceback import print_exc
+import subprocess
 
-class CustomResolved:
-	def resolve(self, request, handler):
-		reply = request.reply()
-		reply.add_answer()
-		return reply
 class Command(BaseCommand):
 
 	help = 'Create DNS blackhole configuration file for dnsmasq.'
 
 	def handle(self, *args, **options):
-		fin = open('/etc/dnsmasq.d/dnsmasq-blackhole.conf', 'w')
 		blacklist = BlacklistedDomain.objects.all()
+		fin = open('/etc/dnsmasq.d/dnsmasq-blackhole.conf', 'w')
 		for domain in blacklist:
 			fin.write('address=/%s/127.0.0.1\n' % domain)
 		fin.close()
+
+		subprocess.run([ 'sudo', '/etc/init.d/avahi-daemon', 'restart' ])
