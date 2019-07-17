0b847cca5ef59be7142c7185d81cf6a17997b024
Corentin Delorme <codelorme@gmail.com>
2019-07-17 15:14:45 +0200

    Update management command msf

--- blacklist/management/commands/msf.py
+++ blacklist/management/commands/msf.py
@@ -6,13 +6,13 @@ class Command(BaseCommand):
 
 	help = 'Run Metasploit Framework .'
 
-	def check_bdf_resource_file():
+	def check_bdf_resource_file(self):
 		return os.path.exists('/home/hx/bdfproxy/bdfproxy_msf_resource.rc')
 
 	def handle(self, *args, **options):
 
-		if not check_bdf_resource_file():
+		if not self.check_bdf_resource_file():
 			print('You need to start BDF Proxy first: python3 manage.py bdf_proxy')
 			return
 
-		subprocess.run([ './scripts/run-metasploit.sh' ], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+		subprocess.run([ './scripts/run-metasploit.sh' ])
