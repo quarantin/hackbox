4221a8376ea99115e986e34b4cf1c7032571ece6
Corentin Delorme <codelorme@gmail.com>
2019-07-17 12:47:21 +0200

    Update scripts/run-service.sh: Handle kill switch

--- scripts/run-service.sh
+++ scripts/run-service.sh
@@ -2,10 +2,14 @@
 
 SLEEP=2
 PYTHON=python3
+KILLSWITCH=/tmp/killswitch
 
 while true; do
 	${PYTHON} manage.py ${@}
 	echo "Exit from django management task: ${PYTHON} manage.py ${@}."
 	echo "Restarting in ${SLEEP} seconds..."
 	sleep ${SLEEP}
+	if [ -f "${KILLSWITCH}" ]; then
+		break
+	fi
 done
