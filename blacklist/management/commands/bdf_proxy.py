cbce3799e018a669e23e8130be28879923968c0b
Corentin Delorme <codelorme@gmail.com>
2019-07-26 20:10:44 +0200

    Update blacklist/management/commands/bdf_proxy.py: Check if ip forwarding is enabled and exit otherwise

--- blacklist/management/commands/bdf_proxy.py
+++ blacklist/management/commands/bdf_proxy.py
@@ -6,7 +6,15 @@ class Command(BaseCommand):
 
 	help = 'Run BDF Proxy.'
 
+	def check_ip_forward(self):
+		fin = open('/proc/sys/net/ipv4/ip_forward', 'r')
+		status = int(fin.read())
+		if status != 1:
+			print('You have to enable IPv4 forwarding. Please run the following command before proceeding:\nsudo sh -c "echo 1" > /proc/sys/net/ipv4/ip_forward')
+			sys.exit()
+
 	def handle(self, *args, **options):
+		self.check_ip_forward()
 		os.chdir('/home/hx/bdfproxy.git')
 		stderrlog = open('bdfproxy-stderr.log', 'w')
 		stdoutlog = open('bdfproxy-stdout.log', 'w')
